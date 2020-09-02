6d830da42e251d26426e5026c958fcf40f9e281a
viri <git@stuff.moe>
2020-09-01 20:13:30 -0600

    gm8exe/asset/**: fix generic import/usage warns

--- gm8exe/src/asset/room.rs
+++ gm8exe/src/asset/room.rs
@@ -5,7 +5,7 @@ use crate::{
     GameVersion,
 };
 use byteorder::{LE, ReadBytesExt, WriteBytesExt};
-use std::io::{self, Seek, SeekFrom};
+use std::io::{self, SeekFrom};
 
 pub const VERSION: u32 = 541;
 
@@ -108,7 +108,7 @@ pub struct ViewFollowData {
 }
 
 impl Asset for Room {
-    fn deserialize_exe(mut reader: impl io::Read + io::Seek, version: GameVersion, strict: bool) -> Result<Self, Error> {
+    fn deserialize_exe(mut reader: impl io::Read + io::Seek, _version: GameVersion, strict: bool) -> Result<Self, Error> {
         let name = reader.read_pas_string()?;
 
         if strict {
@@ -214,7 +214,7 @@ impl Asset for Room {
         })
     }
 
-    fn serialize_exe(&self, mut writer: impl io::Write, version: GameVersion) -> io::Result<()> {
+    fn serialize_exe(&self, mut writer: impl io::Write, _version: GameVersion) -> io::Result<()> {
         writer.write_pas_string(&self.name)?;
         writer.write_u32::<LE>(VERSION)?;
         writer.write_pas_string(&self.caption)?;
